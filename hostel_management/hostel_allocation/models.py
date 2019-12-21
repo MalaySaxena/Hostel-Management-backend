250fe52987074bdde4e59fc33875266487c79b27
Malay <44743128+MalaySaxena@users.noreply.github.com>
2019-12-21 16:08:23 +0530

    models update 1

--- hostel_management/hostel_allocation/models.py
+++ hostel_management/hostel_allocation/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class HostelAllocation(models.Model):
+    hostel_name = models.CharField(max_length=2)
+    hostel_gender = models.CharField(max_length=7)
+    hostel_course = models.CharField(max_length=3)
+    hostel_no_of_floor = models.IntegerField()
+    hostel_no_of_room = models.IntegerField()
+    hostel_no_of_room_available = models.IntegerField()
+    hostel_warden = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.hostel_name
+
+
+class Room(models.Model):
+    type = [('S', 'Single Occupancy'), ('D', 'Double Occupancy'), ('P', 'Reserved for Research Scholars'),('B', 'Both Single and Double Occupancy')]
+    luxury = ['AC','Non AC']
+    room_no = models.IntegerField()
+    room_type = models.CharField(choices=type, max_length=1, default=None)
+    room_luxury = models.CharField(choices=luxury, max_length=10, default=None)
+    is_room_vacant = models.BooleanField(default=False)
+
+    def str(self):
+        return self.room_no
+
+class Student(models.Model):
+    gender_choices = [('M', 'Male'), ('F', 'Female')]
+    year_choices = ['UG1', 'UG2', 'UG3', 'UG4', 'PG1', 'PG1']
+    course_choices = ['CSE','IT','ECE','MECH','CIVIL','EE']
+    student_name = models.CharField(max_length=200, null=True)
+    father_name = models.CharField(max_length=200, null=True)
+    enrollment_no = models.CharField(max_length=10, unique=True, null=True)
+    student_gender = models.CharField(choices=gender_choices,max_length=1,default=None,null=True)
+    student_year = models.CharField(choices=year_choices,max_length=1,default=None,null=True)
+    student_course = models.CharField(choices=course_choices,max_length=8,default=None)
+    room_allotted = models.BooleanField(default=False)
+
+    def str(self):
+        return self.student_name
